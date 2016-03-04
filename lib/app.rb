2f2413f03ad4d30a4d16c9fa1fefeea7a4f54748
Jinah Adam <jinahadam@gmail.com>
2016-03-04 15:41:09 +0800

    filters

--- lib/app.rb
+++ lib/app.rb
@@ -8,8 +8,25 @@ class App < Sinatra::Base
 
   get '/current_week' do
     content_type :json
+
       @parse = Parse.new "https://www.dailyfx.com/calendar"
       @parse.parse_rows.to_json
+  end
+
+  get '/filter' do
+    content_type :json
+
+    country = "usd"
+    importance = "high"
+    if params[:country] != nil && params[:country] != ""
+      country = params[:country]
+    end
+
+    if params[:importance] != nil && params[:importance] != ""
+      importance = params[:importance]
+    end
+      @parse = Parse.new "https://www.dailyfx.com/calendar"
+      @parse.parse_rows.filter(country: country, importance:importance).to_json
   end
 
 end
