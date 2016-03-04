cb1517fb5b0f01a2f3857927b374a333ca94b578
Jinah Adam <jinahadam@gmail.com>
2016-03-04 15:26:13 +0800

    current_week.json

--- lib/app.rb
+++ lib/app.rb
@@ -3,6 +3,10 @@ class App < Sinatra::Base
   require "parse"
 
   get '/' do
+    "hi"
+  end
+
+  get '/current_week' do
     content_type :json
       @parse = Parse.new "https://www.dailyfx.com/calendar"
       @parse.parse_rows.to_json
